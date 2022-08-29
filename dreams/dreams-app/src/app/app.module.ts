4450bbe9796ce6897acce96c4b13a7cfa5eb0b3b
dartaryan <76628877+dartaryan@users.noreply.github.com>
2022-08-29 09:17:22 +0300

    added store from ngrx

--- dreams/dreams-app/src/app/app.module.ts
+++ dreams/dreams-app/src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {StoreModule} from '@ngrx/store';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {AuthModule} from './auth/auth.module';
 
 @NgModule({
@@ -12,9 +13,11 @@ import {AuthModule} from './auth/auth.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AuthModule
+    AuthModule,
+    StoreModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
