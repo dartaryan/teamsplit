b6ffdfb2c851a9bc70704184e09afa52c7357b8b
dartaryan <76628877+dartaryan@users.noreply.github.com>
2023-05-12 22:02:02 +0300

    continue

--- CircusShowApp/src/app/app.module.ts
+++ CircusShowApp/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import {NgModule} from "@angular/core";
 import {AppComponent} from "./app.component";
 import {BrowserModule} from "@angular/platform-browser";
-import { TicketsComponent } from './tickets/tickets.component';
+import {TicketsComponent} from './tickets/tickets.component';
+import {FormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
 
 @NgModule({
   declarations: [AppComponent, TicketsComponent],
-  imports: [BrowserModule],
-  bootstrap: [AppComponent],
+  imports: [BrowserModule, FormsModule, CommonModule],
+  bootstrap: [AppComponent, TicketsComponent],
 })
 
 export class AppModule {
--- CircusShowApp/src/app/tickets/tickets.component.ts
+++ CircusShowApp/src/app/tickets/tickets.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+interface Ticket {
+  show: string;
+  performer: string;
+  image: string;
+}
 
 @Component({
   selector: 'app-tickets',
   templateUrl: './tickets.component.html',
   styleUrls: ['./tickets.component.css']
 })
-export class TicketsComponent {
 
+export class TicketsComponent {
+  tickets: Ticket[] = [
+    {
+      show: "WE",
+      performer: "John",
+      image: "https://cdn.britannica.com/96/198296-050-65D1A810/Clowns-tour-Ringling-Bros-Barnum-Atlanta-2017.jpg"
+    },
+    {
+      show: "Yes",
+      performer: "Phil",
+      image: "https://cdn.britannica.com/96/198296-050-65D1A810/Clowns-tour-Ringling-Bros-Barnum-Atlanta-2017.jpg"
+    },
+  ]
+
+  isShowing: boolean = true;
+
+  constructor() {
+  }
+
+  ngOnInit()
+    :
+    void {
+  }
 }
